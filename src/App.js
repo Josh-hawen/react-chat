855dae1d6714fdb828cfd350c01f8507e4d99319
Josh-hawen <josh@i.ua>
2018-02-26 02:48:22 +0200

    day two, try to code visual

--- src/App.js
+++ src/App.js
@@ -1,13 +1,216 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
+import Avatar from 'material-ui/Avatar';
+import ImageIcon from 'material-ui-icons/Image';
+import WorkIcon from 'material-ui-icons/Work';
+import BeachAccessIcon from 'material-ui-icons/BeachAccess';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Icon from 'material-ui/Icon';
+import PropTypes from 'prop-types';
 
-class App extends Component {
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: '100vh',
+    zIndex: 1,
+    overflow: 'hidden',
+  },
+  sidebar: {
+    position: 'fixed',
+    width: 320,
+    left: 0,
+    top: 0,
+  },
+  list: {
+    position: 'fixed',
+    top: 64,
+    cursor: 'pointer',
+    'overflow-y': 'scroll',
+    width: 320,
+    maxHeight: '100vh',
+    borderTop: '1px solid #999',
+  },
+  ListItemHover: {
+    backgroundColor: '#999',
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    
+  },
+  appBar: {
+    position: 'absolute',
+    width: `calc(100% - 320px)`,
+  },
+  'appBar-left': {
+    marginLeft: 320,
+  },
+  'appBar-right': {
+    marginRight: 320,
+  },
+  drawerHeader: theme.mixins.toolbar,
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: 320,
+    height: 'calc(100% - 56px)',
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+    },
+  },
+  textField: {
+    position: 'fixed',
+    marginLeft: theme.spacing.unit*3,
+    marginRight: theme.spacing.unit*3,
+    marginBottom: theme.spacing.unit,
+    marginTop: theme.spacing.unit,
+    width: 280,
+    top: 0,
+    left: 0,
+  },
+});
+
+
+
+class App extends React.Component {
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <h1>Chat</h1>
-      </div>
+      <div className={classes.root}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="title" color="inherit" noWrap>
+                DogeCodes React Chat
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <main className={classes.sidebar}>
+          <TextField
+                id="search"
+                label="Search chats..."
+                type="search"
+                className={classes.textField}
+                margin="normal"
+              />
+          <div className={classes.list}>
+      <List>
+        <ListItem>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+          <ListItemText primary="Work" secondary="Jan 7, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+          <ListItemText primary="Vacation" secondary="July 20, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+          <ListItemText primary="Work" secondary="Jan 7, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+          <ListItemText primary="Vacation" secondary="July 20, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+          <ListItemText primary="Work" secondary="Jan 7, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+          <ListItemText primary="Vacation" secondary="July 20, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+          <ListItemText primary="Work" secondary="Jan 7, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+          <ListItemText primary="Vacation" secondary="July 20, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+          <ListItemText primary="Work" secondary="Jan 7, 2014" />
+        </ListItem>
+        <ListItem>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+          <ListItemText primary="Vacation" secondary="July 20, 2014" />
+        </ListItem>
+        
+      </List>
+      
+    </div>
+          </main>
+        </div>   
+        
     );
   }
 }
 
-export default App;
+
+export default withStyles(styles)(App);
+
+
+
+
+
+
+
+
+
